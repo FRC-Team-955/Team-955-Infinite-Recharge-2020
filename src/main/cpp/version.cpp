27801bb05ab7a5db5833d5c20d172542ddfd1c9c
BluePiggy49 <ericacwood@gmail.com>
2020-01-29 11:28:00 -0800

    Version Control Added

---
+++ src/main/cpp/version.cpp
@@ -0,0 +1,77 @@
+#include <version.h>
+
+int Version::SaveVersionNumber(){
+	std::string filename = std::string("/home/lvuser/") + filesnippet + std::string(".txt");
+	std::ifstream file_in;
+	std::ofstream file_out;
+	file_in.open(filename.c_str());
+	std::string data;
+	std::getline(file_in, data);
+	file_out.open(filename.c_str());
+	int returnval;
+	if (data.length() > 0){
+		int data_i = std::atoi(data.c_str());
+		file_out << data_i + 1<<std::endl;
+		returnval = data_i + 1;
+	}
+	else{
+		file_out <<"1"<<std::endl;
+		returnval = 1;
+	}
+	file_in.close();
+	file_out.close();
+	version_number = returnval;
+	return returnval;
+}
+
+void Version::CreateSaveFile(std::string inputstring){ // Create function that takes reference to an ofstream object
+	std::ofstream output_file;
+	std::time_t current_time = time(0);
+	std::string time_string = std::ctime(&current_time); // Parameter takes pointer to time object
+//	std::cout<<std::ctime(&current_time)<<std::endl;
+	std::string time_string_clear = "";
+
+	for (int loop = 0; loop < time_string.length(); loop++)
+	{
+		if (time_string[loop] == ' ' || time_string[loop] == ':'){
+			time_string[loop] = '-';
+		}
+		else if (time_string[loop] == '\n'){
+			time_string[loop] = '.';
+		}
+	}
+	std::stringstream version_ss;
+	version_ss << version_number;
+	time_string_clear = "_V" + version_ss.str() + std::string(time_string) + "txt";
+	std::string filename = std::string("/home/lvuser/") + filesnippet + time_string_clear;
+//	std::cout<<filename<<std::endl;
+//	std::cout<<filename.c_str()<<std::endl;
+	output_file.open(filename.c_str());
+	output_file << inputstring;
+	output_file.close();
+}
+
+void Version::SavePrintStatement(std::string save_string, std::string &storage){
+	std::cout<<save_string<<std::endl;
+	storage = storage + save_string + "\n";
+}
+
+std::string Version::ToString(float integer){
+	std::stringstream int_ss;
+	int_ss << integer;
+	std::string int_s = int_ss.str();
+	return int_s;
+}
+
+void Version::AddToPipeDelimitedFile(std::string topic, std::string value, std::string &storage_topic, std::string &storage_value){
+	std::cout<<topic<<": "<<value<<std::endl;
+	storage_value = storage_value + value + "|";
+	std::string topic_pipe = topic + std::string("|");
+	if (storage_topic.find(topic_pipe) == std::string::npos) {
+		storage_topic = storage_topic + topic_pipe;
+	}
+}
+
+void Version::EndLoop(std::string &storage_value){
+	storage_value = storage_value + "\n";
+}
