ae04e0dcb721e75dc0f93557debc7c64fa6d745d
owen-mcmanus <owen.mcmanus727@gmail.com>
2020-01-30 19:32:50 -0800

    Added Lidar.h

---
+++ src/main/include/Lidar.h
@@ -0,0 +1,25 @@
+#ifndef LIDAR
+#define LIDAR
+
+#include <iostream>
+#include "frc/WPILib.h"
+#include "ctre/Phoenix.h"
+#include <string>
+#include <cmath>
+
+using namespace frc;
+class Lidar {
+   public:
+       Lidar (Counter *lidar) : lidar(lidar) {lidar->SetMaxPeriod(1.0);
+                                              lidar->SetSemiPeriodMode(true);
+                                              lidar->Reset();};
+       int GetDistance(std::string units);
+       bool IsInRange(int low_range, int high_range);
+
+   private:
+        Counter *lidar;
+        int dist;
+        int low_range;
+        int high_range;
+};
+#endif
