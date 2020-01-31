bbae9041fa9109ecab31c9d2e1cb0d685bf892dc
BluePiggy49 <ericacwood@gmail.com>
2020-01-31 10:59:12 -0800

    #1

---
+++ src/main/include/auto.h
@@ -0,0 +1,30 @@
+#ifndef AUTO
+#define AUTO
+
+#include <iostream>
+#include "frc/WPILib.h"
+#include "ctre/Phoenix.h"
+#include <frc/Joystick.h>
+
+#include <string>
+
+#include <frc/TimedRobot.h>
+#include <frc/smartdashboard/SendableChooser.h>
+
+class Auto {
+	public:
+		Auto (TalonSRX *drive_talon_right, TalonSRX *drive_talon_left) : drive_talon_right(drive_talon_right), drive_talon_left(drive_talon_left) {};
+
+		int CalculateEncoderCounts(int length_inches);
+
+
+	private:
+		TalonSRX *drive_talon_right;
+		TalonSRX *drive_talon_left;
+
+		int gear_ratio;
+		const int enc_per_rev_talon = 4096;
+		int wheel_size;
+};
+
+#endif
