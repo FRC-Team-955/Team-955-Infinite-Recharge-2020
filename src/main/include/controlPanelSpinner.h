18d20e5a6614bc84c355cd5bfe4b0af057f4da07
owen-mcmanus <owen.mcmanus727@gmail.com>
2020-02-02 14:24:36 -0800

    #2

--- src/main/include/controlPanelSpinner.h
+++ src/main/include/controlPanelSpinner.h
@@ -12,13 +12,13 @@ using namespace frc;
 //using namespace rev;
 class ControlPanelSpinner {
    public:
-       ControlPanelSpinner (TalonSRX *talon_weelspinner_enc, Solenoid *arm_solenoid_up, Solenoid *arm_solenoid_down) : talon_weelspinner_enc(talon_weelspinner_enc), arm_solenoid_up(arm_solenoid_up), arm_solenoid_down(arm_solenoid_down) {};
+       ControlPanelSpinner (TalonSRX *talon_wheelspinner_enc, Solenoid *arm_solenoid_up, Solenoid *arm_solenoid_down) : talon_wheelspinner_enc(talon_wheelspinner_enc), arm_solenoid_up(arm_solenoid_up), arm_solenoid_down(arm_solenoid_down) {};
         void MoveUp();
         void MoveDown();
         bool PositionControl(bool is_match);
-        bool RotationControl(int color_count);
+        bool RotationControl(int color_count, int max_count);
    private:
-        TalonSRX *talon_weelspinner_enc;  
+        TalonSRX *talon_wheelspinner_enc;  
         Solenoid *arm_solenoid_up;
         Solenoid *arm_solenoid_down;
         bool is_match;
