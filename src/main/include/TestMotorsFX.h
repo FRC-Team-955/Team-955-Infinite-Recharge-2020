50c62f02dddef0d4cb41fd5d5a2eda16d083fb9b
LukeWilliams271 <luckywill79@gmail.com>
2020-02-02 15:29:21 -0800

    #8

---
+++ src/main/include/TestMotorsFX.h
@@ -0,0 +1,36 @@
+#ifndef TESTMOTORSFX
+#define TESTMOTORSFX
+
+#include <ctre/Phoenix.h>
+#include "frc/WPILib.h"
+#include "version.h"
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+#include <sstream>
+using namespace frc;
+using namespace std;
+
+
+class TestMotorsFX {
+	public:
+		TestMotorsFX (TalonFX *talon, Joystick *joystick, Version *print, Timer *timer, string storage, string storage_header) : talon(talon), joystick(joystick), timer(timer), print(print), storage(storage), storage_header(storage_header) {};
+
+		void RunMotor(int output_percentage, int button_forward, int button_backward, bool has_encoder);
+		void SaveVersionNum();		
+		void SaveData();
+		void StartTimer();
+
+	private:
+		TalonFX *talon;
+		Joystick *joystick;
+		Version *print;
+		Timer *timer;
+		string storage;
+		string storage_header;
+};
+
+#endif
