8c66cff71d83cb9dc5c3d50066acf9e5953f03fe
JP Li <44953549+jp-li@users.noreply.github.com>
2020-02-02 16:03:50 -0800

    #5
    Added function - needs re-tuning at comp

---
+++ src/main/cpp/distance.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <distance.h>
+
+double Distance::GetCurrentDistance(const double height_of_field_element_minus_height_of_camera, const double mounting_angle_of_camera_in_radians, double vertical_angle_offset_in_radians) {
+
+    double distance = height_of_field_element_minus_height_of_camera / tan (mounting_angle_of_camera_in_radians + vertical_angle_offset_in_radians);
+    
+    return distance;
+}
