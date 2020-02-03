59025f137e5da82ad493950b83f84db94836eca1
JP Li <44953549+jp-li@users.noreply.github.com>
2020-02-02 16:04:39 -0800

    #5
    Added function, needs re-tuning at comp

---
+++ src/main/include/distance.h
@@ -0,0 +1,13 @@
+#ifndef DISTANCE_H
+#define DISTANCE_H
+#include "Robot.h"
+#include "ctre/Phoenix.h"
+#include <iostream>
+#include "frc/WPILib.h"
+
+class Distance {
+    public:
+    double GetCurrentDistance(const double height_of_field_element_minus_height_of_camera, const double mounting_angle_of_camera_in_radians, double vertical_angle_offset_in_radians);
+};
+
+#endif
