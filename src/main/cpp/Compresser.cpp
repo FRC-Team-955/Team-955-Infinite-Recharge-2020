8be7c15d32d7cfd270d330a77faf1d61a10a158d
owen-mcmanus <owen.mcmanus727@gmail.com>
2020-02-06 20:26:54 -0800

    Added Compresser.cpp

---
+++ src/main/cpp/Compresser.cpp
@@ -0,0 +1,15 @@
+#include "Compresser.h"
+using namespace frc;
+
+void CompresserClass::CompresserOn(){
+	compresser -> Start();
+}
+
+void CompresserClass::CompresserOff(){
+	compresser -> Stop();
+}
+void CompresserClass::CompresserBatteryVoltage(int volts){
+	if(DriverStation::GetInstance().GetBatteryVoltage() < volts){
+		compresser -> Stop();
+	}
+}
