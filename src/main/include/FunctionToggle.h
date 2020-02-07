0c5bb6816c5838a87a71e4e5d931e72ee5afe3e4
owen-mcmanus <owen.mcmanus727@gmail.com>
2020-02-06 18:26:00 -0800

    Add FunctionToggle.h

---
+++ src/main/include/FunctionToggle.h
@@ -0,0 +1,21 @@
+#ifndef FUNCTIONTOGGLE
+#define FUNCTIONTOGGLE
+
+#include <iostream>
+#include "frc/WPILib.h"
+#include "ctre/Phoenix.h"
+#include <string>
+#include <cmath>
+
+using namespace frc;
+class FunctionToggle {
+   public:
+       FunctionToggle (Joystick *joystick) : joystick(joystick) {};
+       bool ToggleFunction();
+   private:
+        Joystick *joystick;
+        bool is_pressed = false;
+        bool toggle = false;
+
+};
+#endif
