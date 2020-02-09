52b2a283b36fef12d1488a9953d03b116d519ad7
JP Li <44953549+jp-li@users.noreply.github.com>
2020-02-09 13:30:33 -0800

    #14

--- src/main/include/UsbCamera.h
+++ src/main/include/UsbCamera.h
@@ -1,29 +1,13 @@
 #ifndef USBCAMERA
 #define USBCAMERA
-
-#include <string>
 #include <frc/WPILib.h>
-#include <ctre/Phoenix.h>
 #include <iostream>
-#include <frc/TimedRobot.h>
-#include <frc/smartdashboard/SendableChooser.h>
-#include <cmath>
+
 using namespace frc;
 
 class UsbCamera{
     public:
     UsbCamera(){};
     void VideoCapture();
-
-
-    private:
-    
-
-
-
-
-
-
-
 };
 #endif
