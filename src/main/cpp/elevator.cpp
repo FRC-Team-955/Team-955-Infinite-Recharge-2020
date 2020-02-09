b3deff405e0a9b02fa3b992cf318e38bd5a73084
JP Li <44953549+jp-li@users.noreply.github.com>
2020-02-09 14:15:49 -0800

    #14

--- src/main/cpp/elevator.cpp
+++ src/main/cpp/elevator.cpp
@@ -1,6 +1,7 @@
 #include "elevator.h"
-//https://www.youtube.com/watch?v=dQw4w9WgXcQ
+
 using namespace frc;
+
 void Elevate::Elevate(){
   yp = joystick->GetRawAxis(1);
   talon_drive_right_enc->Set(ControlMode::PercentOutput,yp);
