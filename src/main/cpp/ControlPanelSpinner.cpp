bea698c3ec315ffac930de58f3f38d0cd460d98a
JP Li <44953549+jp-li@users.noreply.github.com>
2020-02-09 13:51:19 -0800

    #14

--- src/main/cpp/ControlPanelSpinner.cpp
+++ src/main/cpp/ControlPanelSpinner.cpp
@@ -1,31 +1,32 @@
-#include "controlPanelSpinner.h"
- using namespace frc;
+#include "ControlPanelSpinner.h"
 
- bool ControlPanelSpinner::PositionControl(bool is_match){
+using namespace frc;
+
+bool ControlPanelSpinner::PositionControl(bool is_match){
   if(is_match){
     talon_wheelspinner_enc -> Set(ControlMode::PercentOutput, 0);
     return 1;
-  }else{
+  }
+ else{
     talon_wheelspinner_enc -> Set(ControlMode::PercentOutput, .5);
     return 0;
-  }
+     }
  }
-  bool ControlPanelSpinner::RotationControl(int color_count, int max_count){
+bool ControlPanelSpinner::RotationControl(int color_count, int max_count){
     if(color_count <= max_count){
       talon_wheelspinner_enc -> Set(ControlMode::PercentOutput, .5);
       return 1;
-    }else{
+    }
+ else{
       talon_wheelspinner_enc -> Set(ControlMode::PercentOutput, 0);
       return 0;
-    }
+      }
   }
-  void ControlPanelSpinner::MoveDown(){
+void ControlPanelSpinner::MoveDown(){
     arm_solenoid_up ->Set(1);
     arm_solenoid_down ->Set(0);
   }
-  void ControlPanelSpinner::MoveUp(){
+void ControlPanelSpinner::MoveUp(){
     arm_solenoid_down ->Set(1);
     arm_solenoid_up ->Set(0);
-    }
-
-
+  }
