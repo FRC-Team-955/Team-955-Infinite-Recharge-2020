fe4a97279a4bdf94082d0974cfffeb5bb40fdebc
owen-mcmanus <56615953+owen-mcmanus@users.noreply.github.com>
2020-02-09 13:48:42 -0800

    #14

--- src/main/include/Elevator.h
+++ src/main/include/Elevator.h
@@ -13,22 +13,11 @@ using namespace frc;
 class Elevate{
     public:
         Elevate(TalonSRX *talon_drive_right_enc, frc::Joystick * joystick): talon_drive_right_enc(talon_drive_right_enc),joystick(joystick){};
-        double left_drive;
-        double right_drive;
-        bool align;
-        double elev();
+        void Elevate();
     
     private:
         TalonSRX *talon_drive_right_enc;
-        TalonSRX *talon_drive_left_enc;
-        TalonSRX *talon_drive_left_noenc;
-        TalonSRX *talon_drive_right_noenc;
         Joystick *joystick;
-        AHRS *ahrs; 
-        double degree;
-        double vertical_offset;
-        double horizontal_offset;
-        double targetOffsetAngle_Horizontal;
-
+        int yp;
 };
 #endif
