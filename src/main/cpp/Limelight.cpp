e3dc2385d412804c9ab066e0754cdbe085573c25
owen-mcmanus <56615953+owen-mcmanus@users.noreply.github.com>
2020-02-09 13:51:27 -0800

    #14

--- src/main/cpp/Limelight.cpp
+++ src/main/cpp/Limelight.cpp
@@ -3,22 +3,21 @@
 using namespace frc;
 
 void Limelight::RunAlign(double  horizontal_offset) {
-  int xp = joy0->GetRawAxis(4);
-  int yp = joy0->GetRawAxis(1);
-  
+  int xp = joystick->GetRawAxis(4);
+  int yp = joystick->GetRawAxis(1);
   
   talon_drive_right_enc->Set(ControlMode::PercentOutput, .3 * (yp+xp));
   talon_drive_left_enc->Set(ControlMode::PercentOutput, .3 * (xp-yp));
 
-  if ( horizontal_offset < 0.1 and  horizontal_offset > -0.1) {
+  if (horizontal_offset < 0.1 and  horizontal_offset > -0.1) {
     align = true;
     talon_drive_right_enc->Set(ControlMode::PercentOutput, .3 * (yp+xp));
     talon_drive_left_enc->Set(ControlMode::PercentOutput, .3 * (xp-yp));
   }
-  else if ( horizontal_offset > 0.15 and align == false) {
+  else if (horizontal_offset > 0.15 and align == false) {
       talon_drive_left_enc ->Set(ControlMode::PercentOutput,  horizontal_offset/ 45);
   }
-  else if ( horizontal_offset < -.15 and align == false){ 
+  else if (horizontal_offset < -.15 and align == false){ 
     talon_drive_right_enc ->Set(ControlMode::PercentOutput,   horizontal_offset/ 45);
   }
   
