a5fc74b134649cfe0f7211cb4f88f3fa2742e755
owen-mcmanus <56615953+owen-mcmanus@users.noreply.github.com>
2020-02-09 13:42:43 -0800

    #14

--- src/main/include/TestMotorsSRX.h
+++ src/main/include/TestMotorsSRX.h
@@ -11,6 +11,7 @@
 #include <string>
 #include <ctime>
 #include <sstream>
+
 using namespace frc;
 using namespace std;
 
@@ -18,7 +19,6 @@ using namespace std;
 class TestMotorsSRX {
 	public:
 		TestMotorsSRX (TalonSRX *talon, Joystick *joystick, Version *print, Timer *timer, string storage, string storage_header) : talon(talon), joystick(joystick), timer(timer), print(print), storage(storage), storage_header(storage_header) {};
-
 		void RunMotor(float output_percentage, int button_forward, int button_backward, bool has_encoder);
 		void SaveVersionNum();	
 		void SaveData();
