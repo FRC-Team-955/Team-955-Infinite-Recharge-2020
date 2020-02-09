dc387af7875036aedcc5316a6331dc2e83a2384d
owen-mcmanus <56615953+owen-mcmanus@users.noreply.github.com>
2020-02-09 13:41:43 -0800

    #14

--- src/main/include/TestMotorsFX.h
+++ src/main/include/TestMotorsFX.h
@@ -14,11 +14,9 @@
 using namespace frc;
 using namespace std;
 
-
 class TestMotorsFX {
 	public:
 		TestMotorsFX (TalonFX *talon, Joystick *joystick, Version *print, Timer *timer, string storage, string storage_header) : talon(talon), joystick(joystick), timer(timer), print(print), storage(storage), storage_header(storage_header) {};
-
 		void RunMotor(float output_percentage, int button_forward, int button_backward, bool has_encoder);
 		void SaveVersionNum();		
 		void SaveData();
