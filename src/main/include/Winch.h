cdc9134b23029a6df70a80d94d346de67d699169
JP Li <44953549+jp-li@users.noreply.github.com>
2020-02-09 13:33:17 -0800

    #14

--- src/main/include/Winch.h
+++ src/main/include/Winch.h
@@ -3,20 +3,14 @@
 
 #include "frc/WPILib.h"
 #include <iostream>
-#include <cmath>
-#include <string>
 #include "ctre/Phoenix.h"
-#include <frc/smartdashboard/SmartDashboard.h>
 
 class Winch {
 	public:
 		Winch (TalonSRX *talon_winch_enc) :  talon_winch_enc(talon_winch_enc) {};
 		void RaiseWinch(int tics);
 		void LowerWinch();
-
 	private:
 		TalonSRX *talon_winch_enc;
-
 };
-
 #endif
