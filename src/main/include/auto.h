3fa5649c0f920ac958d8dbcbc86b92164fefffe5
BluePiggy49 <ericacwood@gmail.com>
2020-02-09 21:11:46 -0800

    #1, Turning Works but Straight Doesn't

--- src/main/include/auto.h
+++ src/main/include/auto.h
@@ -5,41 +5,57 @@
 #include "frc/WPILib.h"
 #include "ctre/Phoenix.h"
 #include <frc/Joystick.h>
+
 #include <string>
 
 #include <frc/TimedRobot.h>
 #include <frc/smartdashboard/SendableChooser.h>
-#include <frc2/command/CommandHelper.h>
-#include <frc2/command/SequentialCommandGroup.h>
 
 #include <AHRS.h>
 
-class Auto : public frc2::CommandHelper<frc2::SequentialCommandGroup, Auto> {
+class Auto {
 	public:
-		Auto (TalonSRX *drive_talon_right, TalonSRX *drive_talon_left, AHRS *navx) : 
-		drive_talon_right(drive_talon_right), drive_talon_left(drive_talon_left), navx(navx) {};
+		Auto (TalonSRX *drive_talon_right, 
+			TalonSRX *drive_talon_left, 
+			AHRS *navx) : 
+			drive_talon_right(drive_talon_right), 
+			drive_talon_left(drive_talon_left), 
+			navx(navx) {};
 
 		int CalculateEncoderCounts(int length_inches);
-		void MoveStraight(int distance_inches);
+		void MoveStraight(int distance_inches, int &mode);
 		void Turn(int degrees, int &mode);
-		void InitializeNavX();
+		void InitializeNavX(int &mode);
 		int RampSpeed(int velocity, int difference);
+		int StraightRamp(int velocity, int percent_change);
+		void Stop();
+		void ZeroEnc(int &mode);
+		void Pause(int &mode);
+		void Path();
 
-		void ComplexAuto();
+		int target_distance_change = 0;
+		int percent_change = 0;
+		float abs_target_distance_change = 0;
+		float average_abs_positions = 0;
+		int right_velocity = 0;
+		int left_velocity = 0;
+		int mode = 1;
+
 	private:
 		TalonSRX *drive_talon_right;
 		TalonSRX *drive_talon_left;
 		AHRS *navx;
 
+		bool know_gear_ratio = false;
 		int gear_ratio = 6;
-		const int wheel_diameter = 0;
-		const int max_velocity = 0;
-		const int right_forward_direction = 1; // 1 or -1
+		int full_rotation_enc = 1040;
+		const int wheel_diameter = 4;
+		const int max_velocity = 368;
+		const int right_forward_direction = -1; // 1 or -1
 		const int left_forward_direction = 1; // 1 or -1
 		int max_right_velocity = right_forward_direction * max_velocity;
 		int max_left_velocity = left_forward_direction * max_velocity;
-		int right_velocity;
-		int left_velocity;
+
 		int loop = 1;
 };
 
