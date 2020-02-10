1e3278a53959e023f7c4ccbddba6ddab77cea56d
LukeWilliams271 <luckywill79@gmail.com>
2020-02-09 16:46:31 -0800

    Changed for naming conventions, #14

--- src/main/include/settings.h
+++ src/main/include/settings.h
@@ -7,59 +7,48 @@
 const int joy_0_idx = 0;
 
 const int align_button_idx = 3;
-
 const int set_target_button_idx = 2;
 
 const int foward_back_axis_idx = 1;
-
 const int left_right_axis_idx = 4;
 
     //Joystick 1
 
 const int color_wheel_op_1_button_idx = 1;
-
 const int color_wheel_op_2_button_idx = 2;
 
 const int raise_color_wheel_arm_button_idx = 3;
-
 const int lower_color_wheel_arm_button_idx = 4;
 
-const int toggle_rev_shooter_button_idx = 6;
-
+//const int toggle_rev_shooter_button_idx = 6;    Maybe blends into feed_shooter_button
 const int feed_shooter_button_idx = 3;
 
 const int raise_hook_axis_idx = 0;
-
 const int lower_hook_button_idx = 0;
 
 const int pull_winch_in_button_idx = 0;
 
+//itake is either one axis or two buttons
+
 //Talons
 
-const int drivebase_left_front_talon = 1;
+const int talon_drivebase_left_noenc_num = 1;
+const int talon_drivebase_left_enc_num = 2;
+const int talon_drivebase_right_noenc_num = 3;
+const int talon_drivebase_right_enc_num = 4;
 
-const int drivebase_left_back_talon = 2;
+const int talon_shooter_connected_num = 9;
+const int talon_shooter_noconnected_num = 10;
 
-const int drivebase_right_back_talon = 3;
+const int talon_elevator_num = 6;
+const int talon_winch_num = 8;
 
-const int drivebase_right_front_talon = 4;
+const int talon_intake_num = 7;
 
-const int shooter_front_talon = 9;
-
-const int shooter_back_talon = 10;
-
-const int elevtaor_talon = 6;
-
-const int winch_talon = 8;
-
-const int intake_talon = 7;
-
-const int hopper_talon = 5;
+const int talon_hopper_num = 5;
 
 //Solenoid
 
 //
 
-
-
-
+#endif
