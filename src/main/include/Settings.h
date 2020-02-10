c4b323b18e7db76f9ae2898c033434697460c613
LukeWilliams271 <luckywill79@gmail.com>
2020-02-09 16:09:36 -0800

    Added Settings.h

---
+++ src/main/include/Settings.h
@@ -0,0 +1,65 @@
+#ifndef SETTINGS
+#define SETTINGS
+
+//Controls
+    //Joytick 0
+
+const int joy_0_idx = 0;
+
+const int align_button_idx = 3;
+
+const int set_target_button_idx = 2;
+
+const int foward_back_axis_idx = 1;
+
+const int left_right_axis_idx = 4;
+
+    //Joystick 1
+
+const int color_wheel_op_1_button_idx = 1;
+
+const int color_wheel_op_2_button_idx = 2;
+
+const int raise_color_wheel_arm_button_idx = 3;
+
+const int lower_color_wheel_arm_button_idx = 4;
+
+const int toggle_rev_shooter_button_idx = 6;
+
+const int feed_shooter_button_idx = 3;
+
+const int raise_hook_axis_idx = 0;
+
+const int lower_hook_button_idx = 0;
+
+const int pull_winch_in_button_idx = 0;
+
+//Talons
+
+const int drivebase_left_front_talon = 1;
+
+const int drivebase_left_back_talon = 2;
+
+const int drivebase_right_back_talon = 3;
+
+const int drivebase_right_front_talon = 4;
+
+const int shooter_front_talon = 9;
+
+const int shooter_back_talon = 10;
+
+const int elevtaor_talon = 6;
+
+const int winch_talon = 8;
+
+const int intake_talon = 7;
+
+const int hopper_talon = 5;
+
+//Solenoid
+
+//
+
+
+
+
