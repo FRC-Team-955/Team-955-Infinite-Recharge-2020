ed1be74a4e0a17ce78ab5d1c93307c84f3c133b5
JP Li <44953549+jp-li@users.noreply.github.com>
2020-02-11 19:45:01 -0800

    #14

--- src/main/cpp/pid.cpp
+++ src/main/cpp/pid.cpp
@@ -1,7 +1,7 @@
 #include <pid.h>
 #include <iostream>
 
-void PID::PIDTune(TalonSRX * talon_enc,  double p_value, double i_value, double d_value, double f_value){
+void Pid::PIDTune(TalonSRX * talon_enc,  double p_value, double i_value, double d_value, double f_value){
    talon_enc->Config_kP(0, p_value, 0);
    talon_enc->Config_kI(0, i_value, 0);
    talon_enc->Config_kD(0, d_value, 0);
