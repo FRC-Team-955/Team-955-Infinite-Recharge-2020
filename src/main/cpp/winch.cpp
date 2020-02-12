b7995ce75a39d8c4abea8cab5160ddedc05f5e84
owen-mcmanus <owen.mcmanus727@gmail.com>
2020-02-11 19:17:32 -0800

    #14, Add button function

--- src/main/cpp/winch.cpp
+++ src/main/cpp/winch.cpp
@@ -2,10 +2,14 @@
 #include "settings.h"
 using namespace frc;
 
-void Winch::RaiseWinch(int tics){
-	//talon_winch_enc -> Set(ControlMode::Position, tics);
+void Winch::RaiseWinchAxis(){
 	talon_winch_enc -> Set(ControlMode::PercentOutput, joystick -> GetRawAxis(pull_winch_in_axis_idx));
 }
+
+void Winch::RaiseWinchButton(int tics){
+	talon_winch_enc -> Set(ControlMode::Position, tics);
+}
+
 void Winch::LowerWinch(){
 	//talon_winch_enc -> Set(ControlMode::Position, 0);
 }
