1381429ccf3eb87e51bca6b2d87085626654cc6a
robotics <robotics@karl.CSD509J.Net>
2020-02-11 19:52:27 -0800

    #14, elevator_axis.cpp

--- src/main/cpp/elevator_axis.cpp
+++ src/main/cpp/elevator_axis.cpp
@@ -1,12 +1,16 @@
-#include "elevator.h"
+#include "elevator_axis.h"
 #include "settings.h"
 //https://www.youtube.com/watch?v=dQw4w9WgXcQ
 using namespace frc;
 double Elevate::elev(){
   
-  int yp = joy0->GetRawAxis(raise_lower_hook_axis);
-  talon_elevator_enc->Set(ControlMode::PercentOutput,yp);
-
+  int yp = joy0->Get
+  talon_elevator_enc->Set(ControlMode::PercentOutput,yp/3);
+  } else if (yp < 0){
+    talon_elevator_enc -> Set(ControlMode::PercentOutput,yp/10);
+  } else {
+    talon_elevator_enc -> Set(ControlMode::PercentOutput,yp/10);
+  }
 
 
 
