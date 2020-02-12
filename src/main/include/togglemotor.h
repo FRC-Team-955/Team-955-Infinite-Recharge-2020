8e643eda95a7039a1334f466a6fb09e907b4af6d
LordKevin420 <jiangpeng.li2004@gmail.com>
2020-02-11 20:26:11 -0800

    Added togglemotor.h

---
+++ src/main/include/togglemotor.h
@@ -0,0 +1,18 @@
+#ifndef TOGGLEMOTOR
+#define TOGGLEMOTOR
+#include <iostream>
+#include "frc/WPILib.h"
+#include "ctre/Phoenix.h"
+#include <string>
+#include <cmath>
+using namespace frc;
+class ToggleMotor {
+   public:
+   ToggleMotor (Joystick *joystick, TalonSRX *talon) : joystick(joystick), talon(talon) {};
+   void Toggle(double power, bool &motor_check, bool &button_check, int &x);
+       //Public variables and functions
+   private:
+       Joystick *joystick;
+       TalonSRX *talon;
+};
+#endif
