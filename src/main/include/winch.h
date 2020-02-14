62a1e6f78e57fcc8b36404f133ba4eecf0ef8355
owen-mcmanus <56615953+owen-mcmanus@users.noreply.github.com>
2020-02-13 18:38:08 -0800

    #14, Updated indent size

--- src/main/include/winch.h
+++ src/main/include/winch.h
@@ -6,12 +6,12 @@
 #include "ctre/Phoenix.h"
 
 class Winch {
-    public:
-	    Winch (TalonSRX *talon_winch_enc, frc::Joystick *joystick) :  talon_winch_enc(talon_winch_enc), joystick(joystick) {};
+	public:
+		Winch (TalonSRX *talon_winch_enc, frc::Joystick *joystick) :  talon_winch_enc(talon_winch_enc), joystick(joystick) {};
 		void RaiseWinchAxis();
 		void RaiseWinchButton(int tics);
 		void LowerWinch();
-    private:
+	private:
 		TalonSRX *talon_winch_enc;
 		frc::Joystick *joystick;
 };
