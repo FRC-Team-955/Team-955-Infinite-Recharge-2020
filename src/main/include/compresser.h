92bb6311e8c7fef1a934475af0c07899e4b94e86
owen-mcmanus <owen.mcmanus727@gmail.com>
2020-02-13 17:59:45 -0800

    #14, Removed includes and namespace frc

--- src/main/include/compresser.h
+++ src/main/include/compresser.h
@@ -3,20 +3,16 @@
 
 #include "frc/WPILib.h"
 #include <iostream>
-#include <string>
-#include "ctre/Phoenix.h"
-#include <frc/smartdashboard/SmartDashboard.h>
 
-using namespace frc;
 
 class CompresserClass {
 	public:
-		CompresserClass (Compressor *compresser) :  compresser(compresser) {};
+		CompresserClass (frc::Compressor *compresser) :  compresser(compresser) {};
 		void CompresserOff();
 		void CompresserOn();
 		void CompresserBatteryVoltage(int volts);
 	private:
-		Compressor *compresser;
+		frc::Compressor *compresser;
 		int volts;
 };
 
