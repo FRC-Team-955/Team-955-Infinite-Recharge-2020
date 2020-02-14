c8202a7849b5e611312028e0e53d89ca2949b65e
LukeWilliams271 <luckywill79@gmail.com>
2020-02-13 18:38:09 -0800

    #14, fixing the indents

--- src/main/include/intake.h
+++ src/main/include/intake.h
@@ -9,25 +9,24 @@ using namespace frc;
 
 
 class Intake {
-	public:
-		Intake (TalonSRX *deployment_talon, TalonSRX *intake_talon, Joystick *joystick) 
-    : deployment_talon(deployment_talon), intake_talon(intake_talon), joystick(joystick) {};
+    public:
+        Intake (TalonSRX *deployment_talon, TalonSRX *intake_talon, Joystick *joystick) 
+        : deployment_talon(deployment_talon), intake_talon(intake_talon), joystick(joystick) {};
 
-    bool deploy_state = 0;
-    void DeployIntakeSRX(int input_button, int rotation_distance);
-    void DeployIntakePNE(int input_button);
-    bool button_check = 0;
-    bool motor_check = 0;
-    void RunIntake(int input_button, float output_percentage);
-    void ApplyDeploySoftLimits(int forward_limit, int backward_limit);
+        bool deploy_state = 0;
+        void DeployIntakeSRX(int input_button, int rotation_distance);
+        void DeployIntakePNE(int input_button);
+        bool button_check = 0;
+        bool motor_check = 0;
+        void RunIntake(int input_button, float output_percentage);
+        void ApplyDeploySoftLimits(int forward_limit, int backward_limit);
     
     
-	private:
-		TalonSRX *deployment_talon;
-    TalonSRX *intake_talon;
-		Joystick *joystick;
+  	private:
+		    TalonSRX *deployment_talon;
+        TalonSRX *intake_talon;
+		    Joystick *joystick;
     
 };
-
 
 #endif
