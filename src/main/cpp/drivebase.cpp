38aa31a9be9538e26769a3072918d48d61f9ab9b
BluePiggy49 <ericacwood@gmail.com>
2020-02-14 23:24:44 -0800

    #18, unnecessary variables passed in

--- src/main/cpp/drivebase.cpp
+++ src/main/cpp/drivebase.cpp
@@ -10,31 +10,30 @@ left, right, lcalc, rcalc, and x dont need to have a value set in robot.cpp, but
 #include "settings.h"
 using namespace frc;
 
-void DriveBase::Drive(double left, double right, double lcalc, double rcalc, double &multi, double &x){
-  right = (joystick->GetRawAxis(forward_back_drivebase_axis_idx) + joystick->GetRawAxis(left_right_drivebase_axis_idx));
-  left = (-1*joystick->GetRawAxis(forward_back_drivebase_axis_idx) - -1*joystick->GetRawAxis(left_right_drivebase_axis_idx));
-  if (multi > 0 && joystick->GetRawButton(sensitivity_scale_cont_button_idx) == true && x > 10){
-    multi = multi / 3;        //the input is a decimal so it strts on slower mode and goes up to faster mode when divided by 3.... because of maths
-    x = 0;
-  } 
-  else {
-    x += 1;
-  } 
-  if (multi < 0.125){
-    multi = 1;
-  }
-  right = right / multi;
-  left = left / multi;
-  left = std::min(1.0, left);
-  right = std::min(1.0, right);
-  left = std::max(-1.0, left);
-  right = std::max(-1.0, right);
-  rcalc = right;
-  lcalc = left;
-  lcalc = lcalc * 0.75;
-  rcalc = rcalc * 0.75;
-  talon_drivebase_left_connected->Set(ControlMode::PercentOutput, lcalc);
-  talon_drivebase_right_connected->Set(ControlMode::PercentOutput, rcalc);
-  std::cout<<multi<<"  "<<right<<"  "<<left<<"  "<<std::endl;
+void DriveBase::Drive(double &multi, double &x){
+	right = (joystick->GetRawAxis(forward_back_drivebase_axis_idx) + joystick->GetRawAxis(left_right_drivebase_axis_idx));
+	left = (-1*joystick->GetRawAxis(forward_back_drivebase_axis_idx) - -1*joystick->GetRawAxis(left_right_drivebase_axis_idx));
+	if (multi > 0 && joystick->GetRawButton(sensitivity_scale_cont_button_idx) == true && x > 10){
+		multi = multi / 3;        //the input is a decimal so it strts on slower mode and goes up to faster mode when divided by 3.... because of maths
+		x = 0;
+	} 
+	else {
+		x += 1;
+	} 
+	if (multi < 0.125){
+		multi = 1;
+	}
+	right = right / multi;
+	left = left / multi;
+	left = std::min(1.0, left);
+	right = std::min(1.0, right);
+	left = std::max(-1.0, left);
+	right = std::max(-1.0, right);
+	rcalc = right;
+	lcalc = left;
+	lcalc = lcalc * 0.75;
+	rcalc = rcalc * 0.75;
+	talon_drivebase_left_connected->Set(ControlMode::PercentOutput, lcalc);
+	talon_drivebase_right_connected->Set(ControlMode::PercentOutput, rcalc);
+	std::cout<<multi<<"  "<<right<<"  "<<left<<"  "<<std::endl;
 }
-
--- src/main/include/drivebase.h
+++ src/main/include/drivebase.h
@@ -11,12 +11,17 @@ using namespace frc;
 
 class DriveBase {
    public:
-   DriveBase (Joystick *joystick, TalonSRX *talon_drivebase_left_connected, TalonSRX *talon_drivebase_right_connected) : joystick(joystick), talon_drivebase_left_connected(talon_drivebase_left_connected), talon_drivebase_right_connected(talon_drivebase_right_connected) {};
-   void Drive(double left, double right, double lcalc, double rcalc, double &multi, double &x);
-       
-   private:
-       Joystick *joystick;
-       TalonSRX *talon_drivebase_left_connected;
-       TalonSRX *talon_drivebase_right_connected;
+		DriveBase (Joystick *joystick, TalonSRX *talon_drivebase_left_connected, TalonSRX *talon_drivebase_right_connected) : joystick(joystick), talon_drivebase_left_connected(talon_drivebase_left_connected), talon_drivebase_right_connected(talon_drivebase_right_connected) {};
+		void Drive(double &multi, double &x);
+	   
+	private:
+		Joystick *joystick;
+		TalonSRX *talon_drivebase_left_connected;
+		TalonSRX *talon_drivebase_right_connected;
+
+		double left;
+		double right;
+		double lcalc;
+		double rcalc;
 };
 #endif
