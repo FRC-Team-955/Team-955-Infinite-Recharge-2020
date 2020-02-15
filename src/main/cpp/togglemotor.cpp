04c47740a85c268c4242d7b8d9e05c8f97fd059f
LukeWilliams271 <luckywill79@gmail.com>
2020-02-14 17:45:41 -0800

    #14, minor optimization

--- src/main/cpp/togglemotor.cpp
+++ src/main/cpp/togglemotor.cpp
@@ -2,22 +2,19 @@
 
 using namespace frc;
 
-void ToggleMotor::Toggle(double power, bool &motor_check, bool &button_check, int &x) {
-    if (x > 10){
-        if (joystick->GetRawButton(1) == false && button_check == true) {
+void ToggleMotor::Toggle(double power, bool &motor_check, bool &button_check, int button_idx) {
+        if (joystick->GetRawButton(button_idx) == false && button_check == true) {
             button_check = false;
-        } else if (joystick->GetRawButton(1) == true && motor_check == false && button_check == false) {
+        } else if (joystick->GetRawButton(button_idx) == true && motor_check == false && button_check == false) {
             talon->Set(ControlMode::PercentOutput, power); //power is set in robot.cpp-teleopinit
             motor_check = true;
             button_check = true;
             x = 0;
-        } else if (joystick->GetRawButton(1) == true && motor_check == true && button_check == false) {
+        } else if (joystick->GetRawButton(button_idx) == true && motor_check == true && button_check == false) {
             talon->Set(ControlMode::PercentOutput, 0);
             motor_check = false;
             button_check = true;
             x = 0;
         } 
-    } else {
-        x += 1;
     }
-}
+    } 
