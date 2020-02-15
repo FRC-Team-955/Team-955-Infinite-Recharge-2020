cf36e519791f87937df19f8e9e2de0f56a431f05
BluePiggy49 <ericacwood@gmail.com>
2020-02-14 23:23:57 -0800

    #17

--- src/main/cpp/intake.cpp
+++ src/main/cpp/intake.cpp
@@ -9,30 +9,31 @@ using namespace frc;
 
 
 void Intake::DeployIntakePNE()
-{
-    
-        if (joystick->GetRawButton(deploy_intake_button_idx) == false && button_check == true) {
-            button_check = false;
-        } else if (joystick->GetRawButton(deploy_intake_button_idx) == true && motor_check == false && button_check == false) {
-            solenoid_intake_right_0->Set(!solenoid_intake_right_0->Get());
-            solenoid_intake_right_1->Set(!solenoid_intake_right_1->Get());	
-            solenoid_intake_left_0->Set(!solenoid_intake_left_0->Get());
-            solenoid_intake_left_1->Set(!solenoid_intake_left_1->Get());            motor_check = true;
-            button_check = true;
-           
-        } else if (joystick->GetRawButton(deploy_intake_button_idx) == true && motor_check == true && button_check == false) {
-            solenoid_intake_right_0->Set(!solenoid_intake_right_0->Get());
-            solenoid_intake_right_1->Set(!solenoid_intake_right_1->Get());	
-            solenoid_intake_left_0->Set(!solenoid_intake_left_0->Get());
-            solenoid_intake_left_1->Set(!solenoid_intake_left_1->Get());            motor_check = false;
-            button_check = true;
-           
-        }
+{	
+		if (joystick->GetRawButton(deploy_intake_button_idx) == false && button_check == true) {
+			button_check = false;
+		} else if (joystick->GetRawButton(deploy_intake_button_idx) == true && motor_check == false && button_check == false) {
+			solenoid_intake_right_0->Set(!solenoid_intake_right_0->Get());
+			solenoid_intake_right_1->Set(!solenoid_intake_right_1->Get());	
+			solenoid_intake_left_0->Set(!solenoid_intake_left_0->Get());
+			solenoid_intake_left_1->Set(!solenoid_intake_left_1->Get());
+			motor_check = true;
+			button_check = true;
+
+		} else if (joystick->GetRawButton(deploy_intake_button_idx) == true && motor_check == true && button_check == false) {
+			solenoid_intake_right_0->Set(!solenoid_intake_right_0->Get());
+			solenoid_intake_right_1->Set(!solenoid_intake_right_1->Get());	
+			solenoid_intake_left_0->Set(!solenoid_intake_left_0->Get());
+			solenoid_intake_left_1->Set(!solenoid_intake_left_1->Get());            
+			motor_check = false;
+			button_check = true;
+		   
+		}
 }
 
 void Intake::RunIntake(float output_percentage)
 {
-    intake_toggle -> Toggle(run_intake_button_idx, intake_talon);
+	intake_toggle -> Toggle(run_intake_button_idx, intake_talon, output_percentage);
 } 
 
 
--- src/main/include/intake.h
+++ src/main/include/intake.h
@@ -5,30 +5,27 @@
 #include "frc/WPILib.h"
 #include "togglemotor.h"
 
-
-
-
 class Intake {
-    public:
-        Intake (ToggleMotor *intake_toggle, TalonSRX *intake_talon, frc::Solenoid *solenoid_intake_right_0, frc::Solenoid *solenoid_intake_right_1, frc::Solenoid *solenoid_intake_left_0, frc::Solenoid *solenoid_intake_left_1, frc::Joystick *joystick) 
-        : intake_toggle(intake_toggle), intake_talon(intake_talon), solenoid_intake_right_0(solenoid_intake_right_0), solenoid_intake_right_1(solenoid_intake_right_1), solenoid_intake_left_0(solenoid_intake_left_0), solenoid_intake_left_1(solenoid_intake_left_1), joystick(joystick) {};
+	public:
+		Intake (ToggleMotor *intake_toggle, TalonSRX *intake_talon, frc::Solenoid *solenoid_intake_right_0, frc::Solenoid *solenoid_intake_right_1, frc::Solenoid *solenoid_intake_left_0, frc::Solenoid *solenoid_intake_left_1, frc::Joystick *joystick) 
+		: intake_toggle(intake_toggle), intake_talon(intake_talon), solenoid_intake_right_0(solenoid_intake_right_0), solenoid_intake_right_1(solenoid_intake_right_1), solenoid_intake_left_0(solenoid_intake_left_0), solenoid_intake_left_1(solenoid_intake_left_1), joystick(joystick) {};
 
-        bool deploy_state = 0;
-        void DeployIntakePNE();
-        bool button_check = 0;
-        bool motor_check = 0;
-        void RunIntake(float output_percentage);
-    
-    
-    private:
-        ToggleMotor *intake_toggle;
-        TalonSRX *intake_talon;
-        frc::Solenoid *solenoid_intake_right_0;
-        frc::Solenoid *solenoid_intake_right_1;
-        frc::Solenoid *solenoid_intake_left_0;
-        frc::Solenoid *solenoid_intake_left_1;
-        frc::Joystick *joystick;
-    
+		bool deploy_state = 0;
+		void DeployIntakePNE();
+		bool button_check = 0;
+		bool motor_check = 0;
+		void RunIntake(float output_percentage);
+	
+	
+	private:
+		ToggleMotor *intake_toggle;
+		TalonSRX *intake_talon;
+		frc::Solenoid *solenoid_intake_right_0;
+		frc::Solenoid *solenoid_intake_right_1;
+		frc::Solenoid *solenoid_intake_left_0;
+		frc::Solenoid *solenoid_intake_left_1;
+		frc::Joystick *joystick;
+	
 };
 
 #endif
