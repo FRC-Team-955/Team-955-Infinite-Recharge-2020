2ccdbd0c733646ea10f7d3a4173857c45f61ffc1
LukeWilliams271 <luckywill79@gmail.com>
2020-02-14 20:44:13 -0800

    #14, Error fixes

--- src/main/cpp/intake.cpp
+++ src/main/cpp/intake.cpp
@@ -7,68 +7,32 @@
 using namespace frc;
 
 
-/*
-void Intake::DeployIntakeSRX(int rotation_distance)
-{
-    if (joystick -> GetRawButton(input_button))
-    {
-        if (deploy_state == 0)
-        {
-            deployment_talon -> Set(ControlMode::Position, rotation_distance);
-            deploy_state = 1;
-        }
-        if (deploy_state == 1)
-        {
-            deployment_talon -> Set(ControlMode::Position, -rotation_distance);
-            deploy_state = 0;
-            
-        }
-    }
-}
-*/
 
 void Intake::DeployIntakePNE()
 {
-    if (x > 10){
+    
         if (joystick->GetRawButton(deploy_intake_button_idx) == false && button_check == true) {
             button_check = false;
-        } else if (joystick->GetRawButton(1) == true && motor_check == false && button_check == false) {
+        } else if (joystick->GetRawButton(deploy_intake_button_idx) == true && motor_check == false && button_check == false) {
             solenoid_intake_right_0->Set(!solenoid_intake_right_0->Get());
             solenoid_intake_right_1->Set(!solenoid_intake_right_1->Get());	
             solenoid_intake_left_0->Set(!solenoid_intake_left_0->Get());
             solenoid_intake_left_1->Set(!solenoid_intake_left_1->Get());            motor_check = true;
             button_check = true;
-            x = 0;
+           
         } else if (joystick->GetRawButton(deploy_intake_button_idx) == true && motor_check == true && button_check == false) {
             solenoid_intake_right_0->Set(!solenoid_intake_right_0->Get());
             solenoid_intake_right_1->Set(!solenoid_intake_right_1->Get());	
             solenoid_intake_left_0->Set(!solenoid_intake_left_0->Get());
             solenoid_intake_left_1->Set(!solenoid_intake_left_1->Get());            motor_check = false;
             button_check = true;
-            x = 0;
-        } 
-    } else {
-        x += 1;
-    }
+           
+        }
 }
 
 void Intake::RunIntake(float output_percentage)
 {
-    if (joystick -> GetRawButton(run_intake_button_idx))            //Press button_foward to run forward
-    {
-    	intake_toggle -> Toggle(output_percentage, );
-
-    } else           //Dont press to stop the motor (or, like... disable it)
-    {
-        intake_talon -> Set(ControlMode::PercentOutput, 0);
-    }
+    intake_toggle -> Toggle(run_intake_button_idx, intake_talon);
 } 
-void Intake::ApplyDeploySoftLimits(int forward_limit, int backward_limit)
-{
-    deployment_talon -> ConfigForwardSoftLimitThreshold(forward_limit, 10);
-    deployment_talon -> ConfigReverseSoftLimitThreshold(backward_limit, 10);
 
-    deployment_talon -> ConfigForwardSoftLimitEnable(true, 10);
-    deployment_talon -> ConfigReverseSoftLimitEnable(true, 10);
 
-}
