2a0315cdcc7dc081de7bfb5c0250db80da89fc93
owen-mcmanus <owen.mcmanus727@gmail.com>
2020-02-15 09:32:40 -0800

    #14, Fixed Errors

--- src/main/cpp/intake.cpp
+++ src/main/cpp/intake.cpp
@@ -10,7 +10,7 @@ using namespace frc;
 
 void Intake::DeployIntakePNE()
 {	
-		if (joystick->GetRawButton(deploy_intake_button_idx) == false && button_check == true) {
+		/*if (joystick->GetRawButton(deploy_intake_button_idx) == false && button_check == true) {
 			button_check = false;
 		} else if (joystick->GetRawButton(deploy_intake_button_idx) == true && motor_check == false && button_check == false) {
 			solenoid_intake_right_0->Set(!solenoid_intake_right_0->Get());
@@ -28,7 +28,21 @@ void Intake::DeployIntakePNE()
 			motor_check = false;
 			button_check = true;
 		   
-		}
+		}*/
+	if (joystick->GetRawButton(1)){
+    std::cout<<"1 button 1"<<std::endl;
+    solenoid_intake_right_0 -> Set(1);
+    solenoid_intake_right_1 -> Set(0);
+	solenoid_intake_left_0 -> Set(1);
+	solenoid_intake_left_1 -> Set(0);
+  }
+  else {
+    std::cout<<"0 button 1"<<std::endl;
+    solenoid_intake_right_0 -> Set(0);
+    solenoid_intake_right_1 -> Set(1);
+	solenoid_intake_left_0 -> Set(0);
+	solenoid_intake_left_1 -> Set(1); 
+	 }
 }
 
 void Intake::RunIntake(float output_percentage)
