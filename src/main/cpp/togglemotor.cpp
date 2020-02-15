ee227676f5292d45691c33894675c408eede6191
LordKevin420 <jiangpeng.li2004@gmail.com>
2020-02-14 18:34:20 -0800

    Added togglemotor.cpp

--- src/main/cpp/togglemotor.cpp
+++ src/main/cpp/togglemotor.cpp
@@ -1,20 +1,17 @@
 #include "togglemotor.h"
-
+#include "settings.h"
 using namespace frc;
 
-void ToggleMotor::Toggle(double power, bool &motor_check, bool &button_check, int button_idx) {
-        if (joystick->GetRawButton(button_idx) == false && button_check == true) {
-            button_check = false;
-        } else if (joystick->GetRawButton(button_idx) == true && motor_check == false && button_check == false) {
-            talon->Set(ControlMode::PercentOutput, power); //power is set in robot.cpp-teleopinit
-            motor_check = true;
-            button_check = true;
-            x = 0;
-        } else if (joystick->GetRawButton(button_idx) == true && motor_check == true && button_check == false) {
-            talon->Set(ControlMode::PercentOutput, 0);
-            motor_check = false;
-            button_check = true;
-            x = 0;
-        } 
+void ToggleMotor::Toggle(int toggle_motor_button) {
+    if (joystick->GetRawButton(hopper_button_idx) == false && button_check == true) {
+        button_check = false;
+    } else if (joystick->GetRawButton(hopper_button_idx) == true && motor_check == false && button_check == false) {
+        talon->Set(ControlMode::PercentOutput, power); //power is set in robot.cpp-teleopinit
+        motor_check = true;
+        button_check = true;
+    } else if (joystick->GetRawButton(hopper_button_idx) == true && motor_check == true && button_check == false) {
+        talon->Set(ControlMode::PercentOutput, 0);
+        motor_check = false;
+        button_check = true;
     }
-    } 
+}
