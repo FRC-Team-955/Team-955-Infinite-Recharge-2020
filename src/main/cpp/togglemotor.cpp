4558ca8a2b652cf10882c9492b17716834f91648
LordKevin420 <jiangpeng.li2004@gmail.com>
2020-02-14 20:05:01 -0800

    #14, Fixed errors

--- src/main/cpp/togglemotor.cpp
+++ src/main/cpp/togglemotor.cpp
@@ -2,15 +2,15 @@
 #include "settings.h"
 using namespace frc;
 
-void ToggleMotor::Toggle(int toggle_motor_button) {
-    if (joystick->GetRawButton(hopper_button_idx) == false && button_check == true) {
+void ToggleMotor::Toggle(int toggle_motor_button, TalonSRX * talon_toggle) {
+    if (joystick->GetRawButton(toggle_motor_button) == false && button_check == true) {
         button_check = false;
-    } else if (joystick->GetRawButton(hopper_button_idx) == true && motor_check == false && button_check == false) {
-        talon->Set(ControlMode::PercentOutput, power); //power is set in robot.cpp-teleopinit
+    } else if (joystick->GetRawButton(toggle_motor_button) == true && motor_check == false && button_check == false) {
+        talon_toggle->Set(ControlMode::PercentOutput, power); //power is set in robot.cpp-teleopinit
         motor_check = true;
         button_check = true;
-    } else if (joystick->GetRawButton(hopper_button_idx) == true && motor_check == true && button_check == false) {
-        talon->Set(ControlMode::PercentOutput, 0);
+    } else if (joystick->GetRawButton(toggle_motor_button) == true && motor_check == true && button_check == false) {
+        talon_toggle->Set(ControlMode::PercentOutput, 0);
         motor_check = false;
         button_check = true;
     }
