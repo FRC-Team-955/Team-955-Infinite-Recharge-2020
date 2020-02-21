d404f8c8aa170336035e3f103810c1f4f4674b7f
LukeWilliams271 <luckywill79@gmail.com>
2020-02-20 18:44:30 -0800

    #14, Changed the running of the Intake to be a press-and-hold rather than toggle

--- src/main/include/intake.h
+++ src/main/include/intake.h
@@ -3,11 +3,10 @@
 
 #include <ctre/Phoenix.h>
 #include "frc/WPILib.h"
-#include "togglemotor.h"
 
 class Intake {
 	public:
-		Intake (ToggleMotor *intake_toggle, TalonSRX *intake_talon, frc::Solenoid *solenoid_intake_right_0, frc::Solenoid *solenoid_intake_right_1, frc::Solenoid *solenoid_intake_left_0, frc::Solenoid *solenoid_intake_left_1, frc::Joystick *joystick) 
+		Intake (TalonSRX *intake_talon, frc::Solenoid *solenoid_intake_right_0, frc::Solenoid *solenoid_intake_right_1, frc::Solenoid *solenoid_intake_left_0, frc::Solenoid *solenoid_intake_left_1, frc::Joystick *joystick) 
 		: intake_toggle(intake_toggle), intake_talon(intake_talon), solenoid_intake_right_0(solenoid_intake_right_0), solenoid_intake_right_1(solenoid_intake_right_1), solenoid_intake_left_0(solenoid_intake_left_0), solenoid_intake_left_1(solenoid_intake_left_1), joystick(joystick) {};
 
 		bool deploy_state = 0;
@@ -18,7 +17,7 @@ class Intake {
 	
 	
 	private:
-		ToggleMotor *intake_toggle;
+		
 		TalonSRX *intake_talon;
 		frc::Solenoid *solenoid_intake_right_0;
 		frc::Solenoid *solenoid_intake_right_1;
