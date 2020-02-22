3f7f8e83796f2c88688328bdc02651540d727407
LordKevin420 <jiangpeng.li2004@gmail.com>
2020-02-21 19:59:23 -0800

    #14, Fixed the directional issues for drivebase.

--- src/main/include/drivebase.h
+++ src/main/include/drivebase.h
@@ -1,27 +1,23 @@
 #ifndef DRIVEBASE
 #define DRIVEBASE
-
 #include <iostream>
 #include "frc/WPILib.h"
 #include "ctre/Phoenix.h"
 #include <string>
 #include <cmath>
-
+#include "settings.h"
 using namespace frc;
-
 class DriveBase {
    public:
-		DriveBase (Joystick *joystick, TalonSRX *talon_drivebase_left_connected, TalonSRX *talon_drivebase_right_connected) : joystick(joystick), talon_drivebase_left_connected(talon_drivebase_left_connected), talon_drivebase_right_connected(talon_drivebase_right_connected) {};
-		void Drive(double &multi, double &x);
-	   
-	private:
-		Joystick *joystick;
-		TalonSRX *talon_drivebase_left_connected;
-		TalonSRX *talon_drivebase_right_connected;
-
-		double left;
-		double right;
-		double lcalc;
-		double rcalc;
+   DriveBase (Joystick *joy, TalonSRX *talon_drive_right_noenc, TalonSRX *talon_drive_left_noenc) : joy(joy), talon_drive_right_noenc(talon_drive_right_noenc), talon_drive_left_noenc(talon_drive_left_noenc) {};
+   void Drive(double multi);
+       bool multi_check = false; //used to say if the motors should be multiplied by multi
+       bool button_check = false; //prevents multis function from running more than once if the buttons pressed down
+       double right; //power to the right front motor
+       double left; //power to the left front motor
+   private:
+       Joystick *joy;
+       TalonSRX *talon_drive_right_noenc;
+       TalonSRX *talon_drive_left_noenc;
 };
 #endif
