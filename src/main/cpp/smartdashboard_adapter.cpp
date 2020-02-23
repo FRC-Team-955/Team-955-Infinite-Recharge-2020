da59bb5f0f84c61a6ecfaf9d0ea2b47e744f770d
owen-mcmanus <owen.mcmanus727@gmail.com>
2020-02-23 15:23:35 -0800

    #14, Changed to follow naming thing

--- src/main/cpp/smartdashboard_adapter.cpp
+++ src/main/cpp/smartdashboard_adapter.cpp
@@ -2,37 +2,27 @@
 
 using namespace frc;
 
-void  SmartDashboard_Adapter::Get_Slider(int id_slider){
+int SmartDashboardAdapter::GetSlider(int id_slider){
     std::stringstream ss;
     ss<<id_slider;
     id_slider_real = ss.str();
 
-    std::cout<<"Slider: "<<SmartDashboard::GetNumber("DB/Slider "+id_slider_real, 0)<<std::endl;
+    return SmartDashboard::GetNumber("DB/Slider "+id_slider_real, 0);
 
 
 }
 
-void SmartDashboard_Adapter::Get_Button(int  id_button){
+bool SmartDashboardAdapter::GetButton(int  id_button){
     std::stringstream sb;
     sb << id_button;
     id_button_real = sb.str();
 
-    std::cout<<"Button: "<< SmartDashboard::GetNumber("DB/Button "+id_button_real, 0)<<std::endl;
+    return SmartDashboard::GetNumber("DB/Button "+id_button_real, 0);
 
 
 }
 
-/*void SmartDashboard_Class::Get_String(int  id_string){
-    std::stringstream sss;
-    sss << id_string;
-    std::string id_string_real = sss.str();
-
-    //std::cout<<"String: "<< SmartDashboard::GetString("DB/String  "+id_string_real, )<<std::endl;
-
-
-}*/
-
-std::string SmartDashboard_Adapter::ChooseAuto(){
+std::string SmartDashboardAdapter::ChooseAuto(){
     input = SmartDashboard::GetString("DB/String 0", "no_data");
 
     if(input == "F" || input =="f"){ //Five Ball Pickup = F/f ; Three Ball Pickup = T/t ; Complete Auto from Anywhere = C/c ; Simple Backup = S/s
@@ -44,6 +34,6 @@ std::string SmartDashboard_Adapter::ChooseAuto(){
     }else if(input == "S" || input =="s"){
         return "SimpleAuto";
     }
-    
+
     return "";
 }
