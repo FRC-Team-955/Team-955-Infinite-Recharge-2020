691677d9f2e489dd5f26e965ac0706d143458806
Josh <jjxfan@gmail.com>
2020-02-25 17:05:50 -0800

    Changed powers to correct numbers

--- src/main/cpp/elevator_axis.cpp
+++ src/main/cpp/elevator_axis.cpp
@@ -3,12 +3,12 @@
 using namespace frc;
 void Elevate::elevator(){
   double y_axis = joystick->GetRawAxis(0);
-  if(y_axis > 0){
-  talon_elevator_enc->Set(ControlMode::PercentOutput,y_axis/3);
-  } else if (y_axis < 0){
-    talon_elevator_enc -> Set(ControlMode::PercentOutput,y_axis/10);
+  if(y_axis > 0.2){
+  talon_elevator_enc->Set(ControlMode::PercentOutput,0.7);
+  } else if (y_axis < -0.2){
+    talon_elevator_enc -> Set(ControlMode::PercentOutput,-0.6);
   } else {
-    talon_elevator_enc -> Set(ControlMode::PercentOutput,y_axis/10);
+    talon_elevator_enc -> Set(ControlMode::PercentOutput,0);
   }
 
 }
@@ -23,3 +23,4 @@ void Elevate::MSsketch(){
   
     talon_elevator_enc -> Set(ControlMode::Position, target_pos);
 } 
+
