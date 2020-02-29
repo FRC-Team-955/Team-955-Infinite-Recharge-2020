c6490ce0c690f5db3e2f40668d1e6444a9c0aec0
BluePiggy49 <ericacwood@gmail.com>
2020-02-28 16:30:04 -0800

    3 ball auto

--- src/main/cpp/auto.cpp
+++ src/main/cpp/auto.cpp
@@ -7,7 +7,7 @@ int Auto::CalculateEncoderCounts(int length_inches){
 	return length_inches * (1 / (wheel_diameter * PIE)) * full_rotation_enc;
 }
 
-void Auto::MoveStraight(int distance_inches, int &mode){
+void Auto::MoveStraight(int distance_inches, int max_right_velocity, int max_left_velocity, int &mode, bool ramp){
 	target_distance_change = CalculateEncoderCounts(distance_inches);
 	int right_pos = drive_talon_right->GetSelectedSensorPosition(0);
 	int left_pos = drive_talon_left->GetSelectedSensorPosition(0);
@@ -31,8 +31,20 @@ void Auto::MoveStraight(int distance_inches, int &mode){
 	drive_talon_left->Set(ControlMode::Velocity, left_velocity);
 }
 
+void Auto::PickUpBalls(int distance_inches, int &mode){
+	MoveStraight(distance_inches, max_balls_right_velocity, max_balls_left_velocity,mode, true);
+	IntakeOn();
+}
+
+void Auto::Move(int distance_inches, int &mode){
+	MoveStraight(distance_inches, no_pickup_max_right_velocity, no_pickup_max_left_velocity, mode, false);
+	IntakeOff();
+}
+
 void Auto::Turn(int degrees, int &mode){
-	int current_degrees = navx->GetYaw();
+	IntakeOff();
+	current_degrees = navx->GetYaw();
+	if (current_degrees < 0) current_degrees += 360;
 	bool turn_left = degrees < current_degrees;
 	right_velocity = RampSpeed(max_right_turning_velocity, abs(current_degrees - degrees));
 	left_velocity = RampSpeed(max_left_turning_velocity, abs(current_degrees - degrees)) * -1;
@@ -56,8 +68,8 @@ int Auto::RampSpeed(int velocity, int difference){
 }
 
 int Auto::StraightRamp(int velocity, int percent_change){
-	if (percent_change >= 90) return velocity / 5;
-	if (percent_change >= 80) return velocity / 4;
+	if (percent_change >= 90) return velocity / 6;
+	if (percent_change >= 80) return velocity / 5;
 	if (percent_change > 75) return velocity / 3;
 	if (percent_change > 50) return velocity / 2;
 	if (percent_change > 10) return velocity / 4;
@@ -66,12 +78,14 @@ int Auto::StraightRamp(int velocity, int percent_change){
 
 void Auto::InitializeNavX(int &mode){
 	navx->ZeroYaw();
-	mode++;
+	if (navx->GetYaw() == 0) mode++;
+	IntakeOff();
 }
 
 void Auto::ZeroEnc(int &mode){
 	drive_talon_right->SetSelectedSensorPosition(0, 0, 10);
 	drive_talon_left->SetSelectedSensorPosition(0, 0, 10);
+	IntakeOff();
 	Stop();
 	if (drive_talon_right->GetSelectedSensorPosition(0) == 0 && drive_talon_left->GetSelectedSensorPosition(0) == 0) mode++;
 }
@@ -79,6 +93,7 @@ void Auto::ZeroEnc(int &mode){
 void Auto::Stop(){
 	drive_talon_right->Set(ControlMode::PercentOutput, 0);
 	drive_talon_left->Set(ControlMode::PercentOutput, 0);
+	IntakeOff();
 }
 
 void Auto::Pause(int &mode){
@@ -86,7 +101,16 @@ void Auto::Pause(int &mode){
 	mode++;
 }
 
-/*void Auto::Path(){
+void Auto::IntakeOn(){
+	intake_talon->Set(ControlMode::PercentOutput, intake_talon_speed);
+}
+
+void Auto::IntakeOff(){
+	intake_talon->Set(ControlMode::PercentOutput, 0);	
+}
+
+//This picks up 5 when slow
+/*void Auto::ThreeBallPickup(){
 	if (mode == 1) InitializeNavX(mode);
 	if (mode == 2) Turn(135, mode);
 	if (mode == 3) ZeroEnc(mode);
@@ -99,27 +123,53 @@ void Auto::Pause(int &mode){
 	std::cout<<"Mode: "<<mode<<std::endl;
 }*/
 
-void Auto::Path(){
+void Auto::Shoot(){
+	shooter_talon->Set(ControlMode::PercentOutput, -0.8);
+	if (abs(shooter_talon->GetSelectedSensorVelocity(0)) > 16000){
+		hopper_talon->Set(ControlMode::PercentOutput, -0.4);
+	}
+}
+
+void Auto::ThreeBallPickup(){
 	if (mode == 1) InitializeNavX(mode);
-	if (mode == 2) Turn(135, mode);
+	if (mode == 2) Turn(140, mode);
 	if (mode == 3) ZeroEnc(mode);
-	if (mode == 4) MoveStraight(18, mode);
+	if (mode == 4) Move(30, mode);
+	if (mode == 5) ZeroEnc(mode);
+	if (mode == 6) Turn(180, mode);
+	if (mode == 7) ZeroEnc(mode);
+	if (mode == 8) PickUpBalls(178, mode);
+	if (mode == 9) ZeroEnc(mode);
+	if (mode == 10) Move(-185, mode);
+	//if (mode == 11) InitializeNavX(mode);
+	if (mode == 11) Turn(312, mode);
+	if (mode == 12) ZeroEnc(mode);
+	if (mode == 13) Shoot();
+	if (mode == 14) Stop();
+	std::cout<<"Mode: "<<mode<<std::endl;
+}
+
+void Auto::FiveBallPickup(){
+	if (mode == 1) InitializeNavX(mode);
+	if (mode == 2) Turn(14, mode);
+	if (mode == 3) ZeroEnc(mode);
+	if (mode == 4) Move(18, mode);
 	if (mode == 5) ZeroEnc(mode);
 	if (mode == 6) Turn (180, mode);
 	if (mode == 7) ZeroEnc(mode);
-	if (mode == 8) MoveStraight(160, mode);
+	if (mode == 8) PickUpBalls(150, mode);
 	if (mode == 9) InitializeNavX(mode);
-	if (mode == 10) Turn(-5, mode);
+	if (mode == 10) Turn(-15, mode);
 	if (mode == 11) ZeroEnc(mode);
-	if (mode == 12) MoveStraight(10, mode);
+	if (mode == 12) PickUpBalls(10, mode);
 	if (mode == 13) ZeroEnc(mode);
-	if (mode == 14) MoveStraight(-10, mode);
+	if (mode == 14) Move(-10, mode);
 	if (mode == 15) ZeroEnc(mode);
-	if (mode == 16) Turn(5, mode);
+	if (mode == 16) Turn(15, mode);
 	if (mode == 17) ZeroEnc(mode);
-	if (mode == 18) MoveStraight(10, mode);
+	if (mode == 18) PickUpBalls(10, mode);
 	if (mode == 19) ZeroEnc(mode);
-	if (mode == 20) MoveStraight(-10, mode);
+	if (mode == 20) Move(-10, mode);
 	if (mode == 21) ZeroEnc(mode);
 	if (mode == 22) Stop();
 	std::cout<<"Mode: "<<mode<<std::endl;
@@ -127,6 +177,6 @@ void Auto::Path(){
 
 void Auto::MoveOffLine(){
 	if (mode == 1) ZeroEnc(mode);
-	if (mode == 2) MoveStraight(-36, mode);
+	if (mode == 2) MoveStraight(-36, max_balls_right_velocity, max_balls_left_velocity, mode);
 	if (mode == 3) Stop();
 }
