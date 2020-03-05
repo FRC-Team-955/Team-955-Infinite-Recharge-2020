570894c3e179ef04e870574a5e4383a99fcd9a2a
jp-li <jiangpeng.li2004@gmail.com>
2020-03-04 20:47:09 -0800

    #21

--- src/main/cpp/distance.cpp
+++ src/main/cpp/distance.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <distance.h>
 
-double Distance::GetCurrentDistance(const double height_of_field_element_minus_height_of_camera, const double mounting_angle_of_camera_in_radians, double vertical_angle_offset_in_radians) {
-    double distance = height_of_field_element_minus_height_of_camera / tan (mounting_angle_of_camera_in_radians + vertical_angle_offset_in_radians);
+double Distance::GetCurrentDistance(const double height_of_field_element_minus_height_of_camera, double vertical_angle_offset_in_radians) {
+    
+   double angle = DegreesToRadians(RadiansToDegrees(atan(71.25 / 226)) - (4.55));
+   //double angle = 1.1854;
+    double distance = height_of_field_element_minus_height_of_camera / tan (angle + DegreesToRadians(vertical_angle_offset_in_radians));
     return distance;
 }
-int Distance::DistanceToVelocity(double distance){
-   return -0.056 * distance * distance + 7.33568 * distance - 15659.202;
+
+double Distance::RadiansToDegrees(double radians){
+    return radians * (180 / 3.1415);
 }
+
+double Distance::DegreesToRadians(double degrees){
+    return degrees * (3.1415 / 180);
+}
+int Distance::DistanceToVelocity(double distance){
+   return -0.056 * distance * distance + 7.33568 * distance - 15759.202;
+}
+
+//Insert into robot.cpp
+//shooter->SpinMotorVelocity(distance->DistanceToVelocity(distance->GetCurrentDistance(71.25, distance_offset)));
