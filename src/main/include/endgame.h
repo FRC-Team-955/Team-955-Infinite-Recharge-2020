283410a4744683cead9fbb6c6dc395373785d153
owen-mcmanus <owen.mcmanus727@gmail.com>
2020-03-08 14:07:03 -0700

    Fixed Compiler Errors

--- src/main/include/endgame.h
+++ src/main/include/endgame.h
@@ -8,17 +8,22 @@
 #include "winch.h"
 #include "elevator.h"
 #include "Lidar.h"
+//#include "auto.h"
 
 class EndGame {
 	public:
-		EndGame (Elevator *elevator, Winch *winch, Lidar *lidar, frc::Joystick *joystick) :  elevator(elevator), winch(winch), lidar(lidar), joystick(joystick) {};
+		EndGame (Elevator *elevator, Winch *winch, Lidar *lidar, frc::Joystick *joystick, TalonSRX *talon_drive_left, TalonSRX *talon_drive_right) :  elevator(elevator), winch(winch), lidar(lidar), joystick(joystick), talon_drive_left(talon_drive_left), talon_drive_right(talon_drive_right) {};
+		void MoveSlow();
 		void Climb();
+		int CalculateEncoderCounts(int length_inches);
 		int mode = 1;
 	private:
 		Elevator *elevator;
 		Winch *winch;
 		Lidar *lidar;
 		frc::Joystick *joystick;
+		TalonSRX *talon_drive_left;
+		TalonSRX *talon_drive_right;
 		
 		bool is_pressed = false;
 		bool toggle = false;
