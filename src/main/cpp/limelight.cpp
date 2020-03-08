af4db16966ee80cf99b4bacbbbdbefeb502e5a8f
jp-li <jiangpeng.li2004@gmail.com>
2020-03-08 13:30:59 -0700

    AutoAlign

--- src/main/cpp/limelight.cpp
+++ src/main/cpp/limelight.cpp
@@ -1,31 +1,25 @@
 #include <iostream>
 #include <limelight.h>
+#include <settings.h>
 using namespace frc;
 
 void Limelight::RunAlign(double  horizontal_offset) {
-  int xp = joystick->GetRawAxis(4);
-  int yp = joystick->GetRawAxis(1);
   
-  talon_drive_right_enc->Set(ControlMode::PercentOutput, .3 * (yp+xp));
-  talon_drive_left_enc->Set(ControlMode::PercentOutput, .3 * (xp-yp));
-
-  if (horizontal_offset < 0.1 and  horizontal_offset > -0.1) {
+  if (horizontal_offset < 3 and  horizontal_offset > -3) {
     align = true;
-    talon_drive_right_enc->Set(ControlMode::PercentOutput, .3 * (yp+xp));
-    talon_drive_left_enc->Set(ControlMode::PercentOutput, .3 * (xp-yp));
-  }
+    
+  } 
   else if (horizontal_offset > 0.15 and align == false) {
-      talon_drive_left_enc ->Set(ControlMode::PercentOutput,  horizontal_offset/ 45);
+      talon_drive_left_enc ->Set(ControlMode::PercentOutput, -1* horizontal_offset/ 20);
   }
   else if (horizontal_offset < -.15 and align == false){ 
-    talon_drive_right_enc ->Set(ControlMode::PercentOutput,   horizontal_offset/ 45);
+    talon_drive_right_enc ->Set(ControlMode::PercentOutput,   horizontal_offset/ 20);
   }
   
-  if (joystick->GetRawButton(2)==1){
+  
+  if (joystick0->GetRawButton(align_button_idx)==1){
     align = false;
   }
-  if (joystick->GetRawButton(1)==1){
-    align = true;
-  }
-  std::cout<< horizontal_offset<<std::endl;
+   
+  
 }
